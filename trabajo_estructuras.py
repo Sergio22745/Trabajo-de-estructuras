da442cc653b57a8195cd56860a8d476cdcf945dd
Sergio22745 <sg144584@gmail.com>
2026-03-25 21:21:39 -0500

    Refactor memory management and menu structure

--- trabajo_estructuras.py
+++ trabajo_estructuras.py
@@ -1,124 +1,104 @@
 from collections import deque
 
-# ---------------------------
-# MEMORIA ESTÁTICA
-# ---------------------------
-print("=== MEMORIA ESTÁTICA ===")
-
-try:
-    tamano = int(input("Ingrese el tamaño fijo del arreglo: "))
-except:
-    print("Error: debes ingresar un número.")
-    exit()
-
-arreglo_estatico = [0] * tamano
-
-for i in range(tamano):
-    while True:
-        try:
-            valor = int(input(f"Ingrese el valor para la posición {i}: "))
-            arreglo_estatico[i] = valor
-            break
-        except:
-            print("Ingrese un número válido.")
-
-print("Arreglo estático final:", arreglo_estatico)
-
-
-# ---------------------------
-# MEMORIA DINÁMICA
-# ---------------------------
-print("\n=== MEMORIA DINÁMICA ===")
-
-lista_dinamica = []
-
-while True:
-    try:
-        cantidad = int(input("¿Cuántos valores desea agregar?: "))
-        break
-    except:
-        print("Ingrese un número válido.")
-
-for i in range(cantidad):
-    while True:
-        try:
-            valor = int(input(f"Ingrese el valor {i+1}: "))
-            lista_dinamica.append(valor)
-            break
-        except:
-            print("Ingrese un número válido.")
-
-print("Lista dinámica final:", lista_dinamica)
-
-
-# ---------------------------
-# PILA (LIFO)
-# ---------------------------
-print("\n=== PILA (LIFO) ===")
+lista = []
 pila = []
-
-while True:
-    print("\n1. Insertar")
-    print("2. Eliminar")
-    print("3. Ver pila")
-    print("4. Salir")
-
-    opcion = input("Seleccione una opción: ")
-
-    if opcion == "1":
-        try:
-            valor = int(input("Ingrese valor: "))
-            pila.append(valor)
-        except:
-            print("Valor inválido")
-
-    elif opcion == "2":
-        if pila:
-            print("Se eliminó:", pila.pop())
-        else:
-            print("La pila está vacía")
-
-    elif opcion == "3":
-        print("Pila actual:", pila)
-
-    elif opcion == "4":
-        break
-
-    else:
-        print("Opción inválida")
-
-
-# ---------------------------
-# COLA (FIFO)
-# ---------------------------
-print("\n=== COLA (FIFO) ===")
 cola = deque()
 
 while True:
-    print("\n1. Insertar")
-    print("2. Eliminar")
-    print("3. Ver cola")
+    print("\n===== MENÚ PRINCIPAL =====")
+    print("1. Trabajar con LISTA")
+    print("2. Trabajar con PILA")
+    print("3. Trabajar con COLA")
     print("4. Salir")
 
     opcion = input("Seleccione una opción: ")
 
+    # ---------------- LISTA ----------------
     if opcion == "1":
-        try:
-            valor = int(input("Ingrese valor: "))
-            cola.append(valor)
-        except:
-            print("Valor inválido")
+        while True:
+            print("\n--- LISTA ---")
+            print("1. Agregar elemento")
+            print("2. Ver lista")
+            print("3. Volver al menú")
 
+            op = input("Seleccione: ")
+
+            if op == "1":
+                valor = input("Ingrese valor: ")
+                lista.append(valor)
+
+            elif op == "2":
+                print("Lista:", lista)
+
+            elif op == "3":
+                break
+
+            else:
+                print("Opción inválida")
+
+    # ---------------- PILA ----------------
     elif opcion == "2":
-        if cola:
-            print("Se eliminó:", cola.popleft())
-        else:
-            print("La cola está vacía")
+        while True:
+            print("\n--- PILA (LIFO) ---")
+            print("1. Insertar")
+            print("2. Eliminar")
+            print("3. Ver pila")
+            print("4. Volver")
 
+            op = input("Seleccione: ")
+
+            if op == "1":
+                valor = input("Ingrese valor: ")
+                pila.append(valor)
+
+            elif op == "2":
+                if pila:
+                    print("Se eliminó:", pila.pop())
+                else:
+                    print("La pila está vacía")
+
+            elif op == "3":
+                print("Pila:", pila)
+
+            elif op == "4":
+                break
+
+            else:
+                print("Opción inválida")
+
+    # ---------------- COLA ----------------
     elif opcion == "3":
-        print("Cola actual:", list(cola))
+        while True:
+            print("\n--- COLA (FIFO) ---")
+            print("1. Insertar")
+            print("2. Eliminar")
+            print("3. Ver cola")
+            print("4. Volver")
 
+            op = input("Seleccione: ")
+
+            if op == "1":
+                valor = input("Ingrese valor: ")
+                cola.append(valor)
+
+            elif op == "2":
+                if cola:
+                    print("Se eliminó:", cola.popleft())
+                else:
+                    print("La cola está vacía")
+
+            elif op == "3":
+                print("Cola:", list(cola))
+
+            elif op == "4":
+                break
+
+            else:
+                print("Opción inválida")
+
+    # ---------------- SALIR ----------------
     elif opcion == "4":
+        print("Programa finalizado.")
         break
 
     else:
